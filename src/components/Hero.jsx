314269f46514ff296f824c78ca62a8b0bddc7a30
owinogabriel <g73346920@gmail.com>
2025-11-04 23:54:26 +0300

    feat:added the button for th code sample

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,3 +1,4 @@
+import { ChevronDown } from "lucide-react";
 import { useEffect, useState } from "react";
 
 export default function Hero() {
@@ -29,17 +30,34 @@ export default function Hero() {
       <div className="absolute top-20 left-4 sm:left-10 w-48 sm:w-72 h-48 sm:h-72 bg-blue-500/10 rounded-full blur-3xl animate-pulse" />
       <div className="absolute bottom-20 right-4 sm:right-10 w-64 sm:w-96 h-64 sm:h-96 bg-cyan-500/10 rounded-full blur-3xl animate-pulse delay-1000" />
 
-      <div className="relative order-2 w-full">
+      <div className="relative order-2 min-w-lg">
         <div className="relative bg-white/5 backdrop-blur-xl rounded-xl sm:rounded-2xl p-3 sm:p-4 shadow-2xl border border-white/10">
           <div className="bg-gradient-to-br from-gray-900/20 to-gray-800/20 backdrop-blur-sm rounded-lg overflow-hidden h-[280px] sm:h-[350px] lg:h-[450px] border border-white/5">
             {/* IDE HEADER */}
-            <div className="flex items-center justify-between px-3 sm:px-4 py-2 sm:py-3 bg-white/5 blackdrop-blur-sm border-b border-white/5">
+            <div className="flex items-center justify-between px-3 sm:px-4 py-2 sm:py-3 bg-white/5 backdrop-blur-sm border-b border-white/10">
               <div className="flex items-center space-x-2">
                 <div className="flex items-center space-x-1 sm:space-x-2">
                   <div className="w-2 h-2 sm:w-3 sm:h-3 rounded-full bg-red-500" />
                   <div className="w-2 h-2 sm:w-3 sm:h-3 rounded-full bg-yellow-500" />
                   <div className="w-2 h-2 sm:w-3 sm:h-3 rounded-full bg-green-500" />
                 </div>
+                <span>CodeFlow AI</span>
+              </div>
+              <ChevronDown className="w-3 h-3 sm:w-4 sm:h-4 text-gray-400" />
+            </div>
+
+            <div>
+              {/* File Tabs */}
+              <div>
+                <button className="px-3 py-2 backdrop-blur-sm text-xs sm:text-sm rounded-t-lg border bg-white/5 text-gray-300 transition-all duration-200 whitespace-nowrap">
+                  App.jsx
+                </button>
+                <button className="px-3 py-2 backdrop-blur-sm text-xs sm:text-sm rounded-t-lg border bg-white/5 text-gray-300 transition-all duration-200 whitespace-nowrap">
+                  Hero.jsx
+                </button>
+                <button className="px-3 py-2 backdrop-blur-sm text-xs sm:text-sm rounded-t-lg border bg-white/5 text-gray-300 transition-all duration-200 whitespace-nowrap">
+                  Navbar.jsx
+                </button>
               </div>
             </div>
           </div>
